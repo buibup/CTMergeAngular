b182ff7331f0771a236c2ff59727a7bafea6c439
buibup <b5209194@gmail.com>
2017-12-03 01:53:31 +0700

    Initial

---
+++ src/app/patients/patients.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-patients',
+  templateUrl: './patients.component.html',
+  styleUrls: ['./patients.component.css']
+})
+export class PatientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
